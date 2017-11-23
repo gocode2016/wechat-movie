8de2dbcef8a13acff78830235cc9a691c021db91
jinjianhua727 <805001458@qq.com>
2017-11-23 19:18:59 +0800

    更新为测试号

--- config.js
+++ config.js
@@ -4,8 +4,12 @@ var util = require('./libs/util')
 var wechat_file = path.join(__dirname,'./config/wechat.txt')
 var config = {
 	wechat:{
-		appID:'wx7539a9456221a597',
-		appSecret:'00a9a6e1653a79d7e39d4099f91bca45',
+		// 水果摊
+		// appID:'wx7539a9456221a597',
+		// appSecret:'00a9a6e1653a79d7e39d4099f91bca45',
+		// 测试
+		appID:'wxa9ae67a590f66619',
+		appSecret:'62f0ffcd867a525e4a907ea50ac7cfe2',
 		token:'jinjianhua',
 		getAccessToken:function(){
 			return util.readFileAsync(wechat_file)
